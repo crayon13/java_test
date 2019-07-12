8e2f5ff68514285ef77c4101a5ecfa2409020b27
김성겸/플랫폼개발팀 <sungkyum.kim@musinsa.com>
2019-07-12 19:46:58 +0900

    뭔가 합니다.

--- src/test/java/EtcTest.java
+++ src/test/java/EtcTest.java
@@ -1,5 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 
@@ -11,5 +15,21 @@ public class EtcTest {
 
         Assert.assertThat(testString.split(",")[0].trim(), is("a"));
 
+    }
+
+    @Test
+    public void listLamdaReduceTest(){
+        String expectString = "1,2,3,4,5,6,7,8,9";
+
+        List<String> testList = new ArrayList<>();
+        for ( int index = 1; index < 10; index++) {
+            testList.add("" + index);
+        }
+
+        String actualConcatString1 = testList.stream().reduce((s1, s2) -> s1 + "," + s2).get();
+        Assert.assertThat(actualConcatString1, is(expectString));
+
+        String actualConcatString2 = StringUtils.join(testList, ",");
+        Assert.assertThat(actualConcatString2, is(expectString));
     }
 }
