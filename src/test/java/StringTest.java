8fd26e48cfb4237eaa969d319a73f5a97a220670
김성겸/플랫폼개발팀 <sungkyum.kim@musinsa.com>
2019-08-05 10:39:49 +0900

    StringTest수정

--- src/test/java/StringTest.java
+++ src/test/java/StringTest.java
@@ -15,9 +15,21 @@ public class StringTest {
 
     @Test
     public void formatTest() {
-        String testString = String.format("/mab/%s/%s/%s?age=%s&itemList=%s&gender=%s"
-                ,"1", "2", "3", "4", "5", "6");
-        assertEquals(testString, "/mab/1/2/3?age=4&itemList=5&gender=6");
+        assertEquals(
+            String.format(
+                "/mab/%s/%s/%s?age=%s&itemList=%s&gender=%s"
+                ,"1", "2", "3", "4", "5", "6"
+            ),
+            "/mab/1/2/3?age=4&itemList=5&gender=6"
+        );
+
+        assertEquals(
+            String.format("/mab/%d/%s/%d?age=%s&itemList=%d&gender=%s"
+                ,1, "2", 3, "4", 5, "6"
+            ),
+            "/mab/1/2/3?age=4&itemList=5&gender=6"
+        );
+
     }
 
     @Test
