a68bac33cb1292298d647555c6862ab40dfbce10
김성겸 <skkim13@gmail.com>
2019-10-03 18:20:15 +0900

    하나더 추가

--- src/test/java/java8/StreamTest.java
+++ src/test/java/java8/StreamTest.java
@@ -2,7 +2,9 @@ package java8;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import sun.jvm.hotspot.utilities.Assert;
 
+import javax.validation.constraints.AssertTrue;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -55,5 +57,49 @@ public class StreamTest {
         boolean isMobileBrowser = browserList.stream().anyMatch(b -> StringUtils.containsIgnoreCase(b, browser));
 
         assertTrue(isMobileBrowser);
+    }
+
+
+    @Test
+    public void filterAndfindFirstTest() {
+        assertEquals( Sex.seaarhByCode7("8").name(), "K");
+        assertEquals( Sex.seaarhByCode7("4").name(), "F");
+        assertEquals( Sex.seaarhByCode7("").name(), "A");
+        assertEquals( Sex.seaarhByCode7("d").name(), "A");
+
+        assertEquals( Sex.seaarhByCode("8").name(), "K");
+        assertEquals( Sex.seaarhByCode("4").name(), "F");
+        assertEquals( Sex.seaarhByCode("").name(), "A");
+        assertEquals( Sex.seaarhByCode("d").name(), "A");
+    }
+
+    private enum Sex {
+        K("8", "Kids"),
+        F("4", "Female"),
+        A("", "All");
+
+        private String code;
+        private String description;
+
+        Sex(String code, String description) {
+            this.code = code;
+            this.description = description;
+        }
+
+        public static Sex seaarhByCode7(String code) {
+            for ( Sex sex : Sex.values() ) {
+                if ( sex.code.equals(code) )  {
+                    return sex;
+                }
+            }
+
+            return Sex.A;
+        }
+
+
+        public static Sex seaarhByCode(String code) {
+            return Arrays.stream(Sex.values()).filter(sex -> sex.code.equals(code)).findFirst().orElse(A);
+        }
+
     }
 }
