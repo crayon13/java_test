34747f59cab22d5c90b9573272e04fbf69acb027
김성겸/플랫폼개발팀 <sungkyum.kim@musinsa.com>
2019-10-07 15:34:09 +0900

    log.debug 사용 방법 변경

--- src/test/java/search/CodecProcessorTest.java
+++ src/test/java/search/CodecProcessorTest.java
@@ -11,11 +11,16 @@ public class CodecProcessorTest {
     public void convertSoundTest() {
         assertThat(convertSoundex("MaenTuMaen/SeuWeTeuSyeoCheu")).isEqualTo("M535");
         assertThat(convertSoundex("KinPal TiSyeoCheu")).isEqualTo("K514");
+        assertThat(convertSoundex("032c")).isEqualTo("S000");
+        assertThat(convertSoundex("10corsocomo")).isEqualTo("K422");
+
     }
 
     @Test
     public void convertMetaphoneTest() {
         assertThat(converMetaphone("MaenTuMaen/SeuWeTeuSyeoCheu")).isEqualTo("MNTMNSWTSYX");
         assertThat(converMetaphone("KinPal TiSyeoCheu")).isEqualTo("KNPLTSYX");
+        assertThat(converMetaphone("10corsocomo")).isEqualTo("KLSKM");
+
     }
 }
