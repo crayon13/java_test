fe2d65f00abf17c2c8b3a65f83c1c789350bb8b7
김성겸/플랫폼개발팀 <sungkyum.kim@musinsa.com>
2019-12-23 19:52:26 +0900

    String matches 추가

--- src/test/java/RegExpTest.java
+++ src/test/java/RegExpTest.java
@@ -1,12 +1,18 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 public class RegExpTest {
 
     @Test
@@ -51,6 +57,23 @@ public class RegExpTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void aaa() {
+        String replacePattern = "good_search-{}";
+        String managePattern = "^" + StringUtils.replace(replacePattern, "{}", "[1-9].+\\d$");
+
+
+        List<String> indices = Arrays.asList("a_good_search-1",
+                "good_search-php",
+                "good_search-201912112300",
+                "good_search-201812112300",
+                "good_search-201912122300");
+
+        List<String> targetIndices = indices.stream().filter(index -> index.matches(managePattern)).collect(Collectors.toList());
+        log.debug("" + targetIndices.size());
+        log.debug(">" + "good_search-201912122300".matches("^good_search-[1-9].+\\d$"));
     }
 
 
